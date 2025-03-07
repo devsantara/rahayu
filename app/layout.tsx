6760d698fdea5416468beb861d1e4ed795a3f606
edwintantawi <edwintantawi.dev@gmail.com>
2025-03-07 23:34:51 +0700

    chore: setup ui library with custom fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
-import './style.css';
+import '~/lib/style/globals.css';
+
+import { font } from '~/lib/font';
+import { cn } from '~/lib/style/utils';
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -6,7 +9,15 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={cn(
+        font.display.variable,
+        font.sans.variable,
+        font.mono.variable,
+        'antialiased',
+      )}
+    >
       <body>{children}</body>
     </html>
   );
