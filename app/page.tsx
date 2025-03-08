b4f931120cc38986919580a065f3b4a9ef7ba2e8
edwintantawi <edwintantawi.dev@gmail.com>
2025-03-08 17:56:27 +0700

    chore: setup prettier
    - Install prettier as formater
    - Add vscode extension for prettier
    - Add prettier config and ignore
    - Add prettier-plugin-tailwindcss
    - Format all files with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function RootPage() {
   return (
-    <main className="h-screen grid place-content-center">
+    <main className="grid h-screen place-content-center">
       <h1 className="text-center font-display">Rahayu by Devsantara</h1>
     </main>
   );
